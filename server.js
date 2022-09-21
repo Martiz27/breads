847f29ffbf3fa2008899e2f51987ebfbcaddd96d
Charlie <ware.charlie914@gmail.com>
2022-09-20 20:24:07 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
 const breadsController = require('./controllers/breads_controller')
 app.use('/breads', breadsController)
+
 
 app.listen(PORT, () =>{
     console.log('Listening on port', PORT)
