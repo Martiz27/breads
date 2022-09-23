af1838b708127f8529111ae9aa0a9b9f53501aee
Charlie <ware.charlie914@gmail.com>
2022-09-22 18:53:44 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -16,6 +17,11 @@ app.get('/', (req, res) => {
 
 const breadsController = require('./controllers/breads_controller')
 app.use('/breads', breadsController)
+
+//404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+})
 
 
 app.listen(PORT, () =>{
