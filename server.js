1188236324574f9522f04e7ff0bdba7b175a4561
Charlie <ware.charlie914@gmail.com>
2022-09-23 21:01:06 -0700

    create and new routes complete

--- server.js
+++ server.js
@@ -9,12 +9,14 @@ app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.urlencoded({extended: true}))
 
-
+//Routes
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
+//Breads
 const breadsController = require('./controllers/breads_controller')
 app.use('/breads', breadsController)
 
@@ -23,7 +25,7 @@ app.get('*', (req, res) => {
     res.send('404')
 })
 
-
+//LISTEN
 app.listen(PORT, () =>{
     console.log('Listening on port', PORT)
 })
